a54fe944c3dc1ff1fd6f4506ae415a57cc861f47
Olawale Olanrewaju <olanrewajume@yahoo.com>
2025-02-26 16:52:19 +0000

    Setup cypress cucumber and typescript

---
+++ cypress/e2e/features/ui.feature
@@ -0,0 +1,5 @@
+Feature: Sauce demo
+
+Scenario: Login
+    Given I open the website
+    Then I can login
---
+++ cypress/e2e/steps/ui_steps.ts
@@ -0,0 +1,11 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("I open the website", function () {
+    console.log("THIS IS THE HOME PAGE");
+    return "pending";
+});
+
+Then("I can login", function () {
+    console.log("THIS IS THE LOGIN PAGE");
+    return "pending";
+});
